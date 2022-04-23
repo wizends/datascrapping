f45c5a201e93b4959bb3bc3b02a1e4a4cc1943fe
wizends <ricardopsf1@gmail.com>
2022-04-23 13:45:36 -0400

    ejercicio terminado

--- index.js
+++ index.js
@@ -1,40 +1,40 @@
-const { val } = require('cheerio/lib/api/attributes');
 const puppeteer = require('puppeteer');
 
     (async () => {
         const browser = await puppeteer.launch();
         const url = 'https://www.sii.cl/servicios_online/1047-nomina_inst_financieras-1714.html';
-        const objeto = [{"Num":"Num",
-                "razonSocial":"razonSocial",
-                "pais":"pais",
-                "datosInscripcion":"datosInscripcion",
-                "vigenciaHasta": "vigenciaHasta",
-                "datosUltimaActualizacion":"datosUltimaActualizacion",
-                "estado":"estado"}]
         try {
             const page = await browser.newPage();
             await page.goto(url);
             const rawData = await page.evaluate(() => {
-                let data = [];
-                let table = document.getElementById('tabledatasii');
-            
-                
-                for (let i = 1; i < 3; i++) {
-                    let objCells = table.rows.item(i).cells;
+                const table = document.getElementById("tabledatasii");
+                const trLength = table.getElementsByTagName("tr").length;
+                let jsonData = [];
+                let obj = {};
 
-                    let values = [];
-                    let id = i;
+                let htmlToJSON = (index) => {
+                    let tr = table.getElementsByTagName("tr")[index];
+                    let td = tr.getElementsByTagName("td");
+                    let arr = [].map.call( td, td => {
+                        return td.innerHTML;
+                    }).join( ',' );
+                    const data = arr.split(",");
                     
-                    
-                    for (let j = 0; j < objCells.length; j++) {
-                        let text = objCells.item(j).innerHTML;
-                        values.push(text);
-                    }
-                    let d = { values };
-            
-                    data.push(values)
+                    obj = {
+                        "numero":data[0]
+                        ,"razonSocial":data[1]
+                        ,"pais":data[2]
+                        ,"datosInscripcion":data[3]
+                        ,"vigenciaHasta":data[4]
+                        ,"datosUltimaActualizacion":data[5]
+                        ,"estado":data[6]
+                    };
+                    jsonData.push(obj);
+                };
+                for(let i = 1; i < trLength; i++){
+                    htmlToJSON(i);
                 }
-                return data;
+                return jsonData
             });
             console.log(rawData);
         } catch (error) {
